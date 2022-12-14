cb74ab2d8c3f51f511e7767fc6dd423cc4ecf049
Jie Pu <i@jaypu.com>
2022-12-14 17:08:02 +0800

    update pj_push

--- .pj/my_tools.sh
+++ .pj/my_tools.sh
@@ -1,12 +1,20 @@
+# $1, hostname
+# $2, sub dir
 
-# consider: 1.different work dir, 2. differenct host
 CUR_DIR=`pwd`
-pj_rsync(){
-    if [[ `cat /proc/version` == MSYS* ]] ;
-    then
+pj_push(){
         DST_DIR=${CUR_DIR:2}
-    else
-        DST_DIR=$CUR_DIR
-    fi
-rsync -avzuP --delete $CUR_DIR pj@$1:$(dirname $DST_DIR)
+        rsync -avzuP --delete $CUR_DIR pj@$1:$(dirname $DST_DIR)
 }
+
+pj_pull(){
+        DST_DIR=${CUR_DIR}
+        if [ $2 ];then
+                SRC_DIR=${CUR_DIR:2}/$2/*
+                DST_DIR=$DST_DIR/$2
+        else
+                echo "please input subdir after hostname"
+        fi
+        rsync -avzuP pj@$1:$SRC_DIR $DST_DIR
+}
+
