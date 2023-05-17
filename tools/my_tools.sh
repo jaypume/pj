3594f77e605b1e776f833821effbec0c288360d3
Jie Pu <i@jaypu.com>
2023-05-17 19:54:30 +0800

    fix push/pull scripts

--- tools/my_tools.sh
+++ tools/my_tools.sh
@@ -1,20 +1,17 @@
 # $1, hostname
 # $2, sub dir
 
-CUR_DIR=`pwd`
 pj_push(){
-        DST_DIR=${CUR_DIR:2}
-        rsync -avzuP --delete $CUR_DIR pj@$1:$(dirname $DST_DIR)
+        CUR_DIR=`pwd`
+        DST_DIR=${CUR_DIR:10}
+        rsync -avzuP --delete $CUR_DIR $1:~$(dirname $DST_DIR)
 }
 
+# pj_pull gz1 
+# 需要在项目根目录下使用
 pj_pull(){
-        DST_DIR=${CUR_DIR}
-        if [ $2 ];then
-                SRC_DIR=${CUR_DIR:2}/$2/*
-                DST_DIR=$DST_DIR/$2
-        else
-                echo "please input subdir after hostname"
-        fi
-        rsync -avzuP pj@$1:$SRC_DIR $DST_DIR
+        CUR_DIR=`pwd`
+        SRC_DIR=${CUR_DIR:10}
+        rsync -avzuP $1:~$SRC_DIR ../.
 }
 
